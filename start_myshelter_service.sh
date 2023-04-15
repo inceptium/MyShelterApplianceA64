ff2b40cce6ab01b7e776c407820bcca287a52bcc
Antonio <cpmatrix@mac.com>
2023-04-15 15:00:22 +0200

    Update

--- start_myshelter_service.sh
+++ start_myshelter_service.sh
@@ -5,7 +5,7 @@ MEDIAPATH=/root/BaseDati/srv/LCServer
 BASEDATI=/root/BaseDati
 cd /root/BaseDati
 #rm myshelter_srv.log
-cd /root/MyShelterAppliance/MyShelter/
+cd /root/MyShelterApplianceA64/MyShelter/
 sleep 15
 SERIALNO=$(cat "/root/BaseDati/etc/serial.no") 
 MSG=0
@@ -16,9 +16,9 @@ while [ $INCEPTIUM_ACTIVE -eq 1 ];
 do
     cp /root/BaseDati/temp/LCimp_new.rsd /root/BaseDati/LCimp.rsd
     #java -Xms128m -Xmx256m  -XX:+UseLinuxPosixThreadCPUClocks -agentpath:/root/profiling/lib/deployed/jdk16/linux-amd64/libprofilerinterface.so=/root/profiling/lib,5140  -cp "InceptiumServices.jar:/root/InceptiumLinuxAppliance/InceptiumServices/lib/*:/root/IncStorage/InceptiumAppStore/*" inceptiumserver.core.server.IncServer noswing updatedatabase
-        java -Xms128m -Xmx512m -cp "myshelter.jar:/root/MyShelterAppliance/MyShelter/lib/*" myshelter.Main noswing $MEDIAPATH $BASEDATI /root/MyShelterAppliance/MyShelter/CustomLib eth0 eth1 >> $LOGFILE 2>&1
+        java -Xms128m -Xmx512m -cp "myshelter.jar:/root/MyShelterApplianceA64/MyShelter/lib/*" myshelter.Main noswing $MEDIAPATH $BASEDATI /root/MyShelterAppliance/MyShelter/CustomLib eth0 eth1 >> $LOGFILE 2>&1
         MSG=$?
-        echo "0" > /root/BaseDati/etc/vpn/led/led2
+        #echo "0" > /root/BaseDati/etc/vpn/led/led2
         echo "Messaggio ricevuto: $MSG" >>  $LOGFILE
         echo "MSG: $MSG" >> $LOGFILE
         RESTARTCOUNT=`expr $RESTARTCOUNT + 1`
